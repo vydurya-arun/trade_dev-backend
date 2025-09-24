f08e6dd60af16d32d94f5f392e5c083fe83dc77e
ARUN KUMAR.A <arun@vydurya.in>
2025-09-24 13:10:55 +0530

    commit

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -220,7 +220,7 @@ export const logout = async (req, res) => {
       secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
     });
-    res.cookie("refreshToken", refreshToken, {
+    res.clearCookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
