df5bb1f1308b0408d3c8811b209b00f8215506c3
ARUN KUMAR.A <arun@vydurya.in>
2025-09-30 19:44:43 +0530

    commit

--- src/route/chargesRoutes.js
+++ src/route/chargesRoutes.js
@@ -1,13 +1,14 @@
 // routes/chargesRoutes.js
 import express from "express";
 import { createCharges, deleteCharge, getCharges, updateCharges } from "../controller/chargesController.js";
+import { authMiddleware } from "../middileware/authMiddleware.js";
 
 const chargeRouter = express.Router();
 
 
-chargeRouter.post("/", createCharges);
-chargeRouter.get("/", getCharges);
-chargeRouter.put("/", updateCharges);
-chargeRouter.delete("/", deleteCharge);
+chargeRouter.post("/",authMiddleware, createCharges);
+chargeRouter.get("/",authMiddleware, getCharges);
+chargeRouter.put("/",authMiddleware, updateCharges);
+chargeRouter.delete("/",authMiddleware, deleteCharge);
 
 export default chargeRouter;
