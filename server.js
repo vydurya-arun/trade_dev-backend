a8d9fe445e05cb6b5b86868240a9e0f8933810ff
ARUN KUMAR.A <arun@vydurya.in>
2025-10-08 12:17:28 +0530

    commit

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors({
-  origin:[ "http://localhost:3000"],
+  origin: process.env.CROS_ORGINS,
   credentials: true,             
 }));
 
